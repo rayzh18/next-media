2d754753db8311f252a8a7e83f85ab6b6c745d8d
sbmose <superbright43@gmail.com>
2021-05-25 15:11:33 -0700

    Added functionality to hamburge icon in order to open or close the  the side navbar

--- components/HBOProvider.js
+++ components/HBOProvider.js
@@ -13,9 +13,21 @@ export function HBOProvider({children}){
         setUser(e.target.value)
         console.log(user)
     }
+    const [sideNavOpen, setSideNavOpenAction] = useState(false)
+    const [accountModalOpen, setAccountModalOpenAction] = useState(false)
+
+
     return(
         <StateContext.Provider
-            value={{user, createUserAction, defaultUserImg}}
+            value={{
+                user, 
+                createUserAction, 
+                defaultUserImg,
+                sideNavOpen,
+                setSideNavOpenAction,
+                accountModalOpen,
+                setAccountModalOpenAction
+            }}
         >
             {children}
         </StateContext.Provider>
--- components/UI/Header/Header.js
+++ components/UI/Header/Header.js
@@ -1,11 +1,13 @@
+import { useStateContext } from "../../HBOProvider"
 import Account from "../Account/Account"
 import SearchModal from "../SearchModal/SearchModal"
 
 
 const Header = (props) => {
+  const globalState = useStateContext()
   return(<header className="top-header">
     <div className="top-header__left-side">
-      <div className="top-header__menu-btn">
+      <div className="top-header__menu-btn" onClick={() => globalState.setSideNavOpenAction(true)}>
         <i className="fas fa-bars"/>
       </div>
       <div className="top-header__search-btn">
@@ -13,7 +15,7 @@ const Header = (props) => {
       </div>
     </div>
     <div className="top-header__logo"></div>
-    <div className="top-header__account">
+    <div className="top-header__account" onClick={() => globalState.setAccountModalOpenAction(true)}>
       <img src="https://uifaces.co/our-content/donated/vIqzOHXj.jpg" className="top-header__user-img"/>
       <div className="top-header__user-name">Bryant</div>
     </div>
