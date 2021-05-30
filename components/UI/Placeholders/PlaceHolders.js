3ab58677b468b13b3f6796154d5f3e82a40252ea
sbmose <superbright43@gmail.com>
2021-05-30 14:45:40 -0700

    Added changes to document file

---
+++ components/UI/Placeholders/PlaceHolders.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
+
+const Placeholders = (props) => {
+  const [loadingData, setLoadingData] = useState(true)  
+  const [movies, setMoviesData] = useState([])
+
+ 
+  return (
+    <div className={`media-row ${props.type}`}>
+      <h3 className="media-row__title">{props.title}</h3>
+      <div className="media-row__thumbnails">
+
+      <div className="media-row__thumbnail-skeleton">
+        <div className="media-row__thumbnail-skeleton-img"></div>
+      </div>
+      <div className="media-row__thumbnail-skeleton">
+        <div className="media-row__thumbnail-skeleton-img"></div>
+      </div>
+      <div className="media-row__thumbnail-skeleton">
+        <div className="media-row__thumbnail-skeleton-img"></div>
+      </div>
+      <div className="media-row__thumbnail-skeleton">
+        <div className="media-row__thumbnail-skeleton-img"></div>
+      </div>
+      <div className="media-row__thumbnail-skeleton">
+        <div className="media-row__thumbnail-skeleton-img"></div>
+      </div>
+      <div className="media-row__thumbnail-skeleton">
+        <div className="media-row__thumbnail-skeleton-img"></div>
+      </div>
+      <div className="media-row__thumbnail-skeleton">
+        <div className="media-row__thumbnail-skeleton-img"></div>
+      </div>
+      <div className="media-row__thumbnail-skeleton">
+        <div className="media-row__thumbnail-skeleton-img"></div>
+      </div>
+     
+      </div>
+    </div>
+  );
+};
+
+const Skeleton = () => {
+  return (
+    <div className="media-row__thumbnail-skeleton">
+      <div className="media-row__thumbnail-skeleton-img"></div>
+    </div>
+  );
+};
+
+export default Placeholders;
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import MediaRow from "../components/UI/MediaRow/MediaRow";
 import { useRouter } from "next/router";
 import AuthCheck from "../components/AuthCheck";
 import LazyLoad from "react-lazyload";
-
+import Placeholders from '../components/UI/Placeholders/PlaceHolders'
 export default function Home() {
   const globalState = useStateContext();
   const router = useRouter();
@@ -18,22 +18,22 @@ export default function Home() {
   return AuthCheck(
     <MainLayout>
       <FeaturedMedia />
-      <LazyLoad height={680} offset={-200} placeholder={<h1>Loading</h1>}>
+      <LazyLoad offset={-400} placeholder={<Placeholders title="Movies" type="large-v" />}>
         <MediaRow title="Movies" type="large-v" endpoint="api/movies/234" />
       </LazyLoad>
-      <LazyLoad height={333} offset={-200}>
+      <LazyLoad offset={-400} placeholder={<Placeholders title="Movies" type="large-v" />}>
         <MediaRow title="Series" type="small-h" endpoint="api/movies/234" />
       </LazyLoad>
-      <LazyLoad height={440} offset={-200}>
+      <LazyLoad offset={-400} placeholder={<Placeholders title="Movies" type="large-v" />}>
         <MediaRow title="Action" type="small-v" endpoint="api/movies/234" />
       </LazyLoad>
-      <LazyLoad height={200}>
+      <LazyLoad offset={-400} placeholder={<Placeholders title="Movies" type="large-v" />}>
         <MediaRow title="Horror" type="small-v" endpoint="api/movies/234" />
       </LazyLoad>
-      <LazyLoad height={200}>
+      <LazyLoad offset={-400} placeholder={<Placeholders title="Movies" type="large-v" />}>
         <MediaRow title="Animations" type="large-h" endpoint="api/movies/234" />
       </LazyLoad>
-      <LazyLoad height={200}>
+      <LazyLoad offset={-400} placeholder={<Placeholders title="Movies" type="large-v" />}>
         <MediaRow title="Sci-fi" type="small-v" endpoint="api/movies/234" />
       </LazyLoad>
     </MainLayout>
